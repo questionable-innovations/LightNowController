d8791370c3821bd4fc7b5f431714396193d9bf55
Jasper M-W <contact@jmw.nz>
2025-04-16 15:41:43 +1200

    Pin definitions

---
+++ src/pins.cpp
@@ -0,0 +1,16 @@
+#include "pins.h"
+
+const int R_DISP_SCK_PIN = 4;
+const int R_DISP_SDA_PIN = 5;
+const int R_INPUT_PIN = 40;
+
+const int G_DISP_SCK_PIN = 6;
+const int G_DISP_SDA_PIN = 7;
+const int G_INPUT_PIN = 39;
+
+const int B_DISP_SCK_PIN = 8;
+const int B_DISP_SDA_PIN = 9;
+const int B_INPUT_PIN = 38;
+
+const int MASTER_INPUT_PIN = 18;
+const int MASTER_POWER_PIN = 21;
---
+++ src/pins.h
@@ -0,0 +1,17 @@
+#pragma once
+
+extern const int R_DISP_SCK_PIN;
+extern const int R_DISP_SDA_PIN;
+extern const int R_INPUT_PIN;
+
+extern const int G_DISP_SCK_PIN;
+extern const int G_DISP_SDA_PIN;
+extern const int G_INPUT_PIN;
+
+extern const int B_DISP_SCK_PIN;
+extern const int B_DISP_SDA_PIN;
+extern const int B_INPUT_PIN;
+
+extern const int MASTER_INPUT_PIN;
+extern const int MASTER_POWER_PIN;
+
